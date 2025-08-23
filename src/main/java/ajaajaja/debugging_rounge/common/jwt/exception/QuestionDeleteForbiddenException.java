5ebdefb3c0f78b7d62401a3bcf541d4acf7af4bc
sonchanghyeon <cson90563@gmail.com>
2025-08-24 03:47:28 +0900

    refactor: Question 관련 예외 위치 변경

--- src/main/java/ajaajaja/debugging_rounge/common/jwt/exception/QuestionDeleteForbiddenException.java
+++
@@ -1,9 +0,0 @@
-package ajaajaja.debugging_rounge.common.jwt.exception;
-
-import ajaajaja.debugging_rounge.common.exception.ErrorCode;
-
-public class QuestionDeleteForbiddenException extends CustomAuthorizationException{
-    public QuestionDeleteForbiddenException() {
-        super(ErrorCode.QUESTION_DELETE_FORBIDDEN);
-    }
-}
--- src/main/java/ajaajaja/debugging_rounge/common/jwt/exception/QuestionUpdateForbiddenExceptionCustom.java
+++
@@ -1,9 +0,0 @@
-package ajaajaja.debugging_rounge.common.jwt.exception;
-
-import ajaajaja.debugging_rounge.common.exception.ErrorCode;
-
-public class QuestionUpdateForbiddenExceptionCustom extends CustomAuthorizationException {
-    public QuestionUpdateForbiddenExceptionCustom() {
-        super(ErrorCode.QUESTION_UPDATE_FORBIDDEN);
-    }
-}
